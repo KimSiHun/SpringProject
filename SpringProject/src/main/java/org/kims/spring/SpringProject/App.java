20f01a025da53f3df8eec0e9de269ed70a88bdc9
sihun <paradaise110@hanmail.net>
2017-01-16 16:24:56 +0900

    adxc

---
+++ SpringProject/src/main/java/org/kims/spring/SpringProject/App.java
@@ -0,0 +1,19 @@
+package org.kims.spring.SpringProject;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+	public static void main(String[] args)
+	{
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The Current local time is: "+ currentTime);
+		
+	}
+	
+}
